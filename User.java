7c34f1baa9d6b2a202cfbe76d7cfc13733bef0a4
Gowtham Devaraj <gowthamdevaraj18@gmail.com>
2025-12-24 15:25:29 +0530

    m1() is executed

--- User.java
+++ User.java
@@ -1,3 +1,17 @@
+<<<<<<< Updated upstream
 public void m2(){
 	//Business logic 
+=======
+Class User{
+	
+	int uid;
+	String uname;
+	
+	//task - 230
+	public void m1(){
+		System.out.println("m1() is executing...");
+		//logic
+	}
+	
+>>>>>>> Stashed changes
 }
